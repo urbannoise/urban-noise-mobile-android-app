97364155b85092d0546c49eb3608f2397f7470c8
Madalin Valceleanu <devmobile@vmadalin.com>
2019-11-02 13:48:46 +0100

    Integrate project with Firebase

--- app/src/main/java/com/urban/noise/MainActivity.kt
+++ app/src/main/java/com/urban/noise/MainActivity.kt
@@ -2,11 +2,15 @@ package com.urban.noise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.crashlytics.android.Crashlytics
+import com.google.firebase.FirebaseApp
+import io.fabric.sdk.android.Fabric
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Fabric.with(this, Crashlytics())
+        FirebaseApp.initializeApp(this)
     }
 }
