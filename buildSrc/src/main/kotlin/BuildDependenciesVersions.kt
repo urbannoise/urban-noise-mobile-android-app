9538756f91b67487b5a249a9b791a31ed0d2b3d7
Madalin Valceleanu <devmobile@vmadalin.com>
2019-11-03 15:39:49 +0100

    Add buildSrc module

---
+++ buildSrc/src/main/kotlin/BuildDependenciesVersions.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2019 vmadalin.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Configuration version of all build dependencies
+ */
+object BuildDependenciesVersions {
+    const val KOTLIN = "1.3.50"
+    const val APPCOMPAT = "1.1.0"
+    const val MATERIAL = "1.1.0-beta01"
+    const val COROUTINES = "1.3.2"
+    const val ROOM = "2.2.0"
+    const val RECYCLE_VIEW = "1.1.0-alpha06"
+    const val NAVIGATION = "2.1.0"
+    const val LIFECYCLE = "2.1.0"
+    const val CORE_KTX = "1.1.0"
+    const val FRAGMENT_KTX = "1.2.0-alpha02"
+    const val CONSTRAIN_LAYOUT = "1.1.3"
+    const val PAGING = "2.1.0"
+    const val DAGGER = "2.24"
+    const val TIMBER = "4.7.1"
+    const val LOGGING = "4.2.2"
+    const val MOSHI = "1.8.0"
+    const val GLIDE = "4.10.0"
+    const val CRASHLYTICS = "2.10.1"
+    const val LEAKCANARY = "2.0-beta-2"
+    const val DATABINDING = "3.1.4"
+    const val FIREBASE = "17.2.1"
+    const val PLAY_CORE = "1.6.3"
+    const val GMS_MAPS = "17.0.0"
+
+    // Tests
+    const val JUNIT = "4.12"
+    const val ROBOELECTRIC = "4.3"
+    const val MOCKITO = "2.2.0"
+    const val ASSERTJ = "3.13.2"
+    const val ESPRESSO = "3.2.0"
+    const val RUNNER = "1.2.0"
+    const val RULES = "1.2.0"
+}
---
+++ buildSrc/src/main/kotlin/dependencies/Dependencies.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2019 vmadalin.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dependencies
+
+/**
+ * Project dependencies, makes it easy to include external binaries or
+ * other library modules to build.
+ */
+object Dependencies {
+    const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${BuildDependenciesVersions.KOTLIN}"
+    const val APPCOMPAT = "androidx.appcompat:appcompat:${BuildDependenciesVersions.APPCOMPAT}"
+    const val MATERIAL = "com.google.android.material:material:${BuildDependenciesVersions.MATERIAL}"
+    const val COROUTINES = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${BuildDependenciesVersions.COROUTINES}"
+    const val COROUTINES_ANDROID = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${BuildDependenciesVersions.COROUTINES}"
+    const val ROOM = "androidx.room:room-runtime:${BuildDependenciesVersions.ROOM}"
+    const val ROOM_KTX = "androidx.room:room-ktx:${BuildDependenciesVersions.ROOM}"
+    const val RECYCLE_VIEW = "androidx.recyclerview:recyclerview:${BuildDependenciesVersions.RECYCLE_VIEW}"
+    const val NAVIGATION_FRAGMENT = "androidx.navigation:navigation-fragment-ktx:${BuildDependenciesVersions.NAVIGATION}"
+    const val NAVIGATION_UI = "androidx.navigation:navigation-ui-ktx:${BuildDependenciesVersions.NAVIGATION}"
+    const val LIFECYCLE_EXTENSIONS = "androidx.lifecycle:lifecycle-extensions:${BuildDependenciesVersions.LIFECYCLE}"
+    const val LIFECYCLE_VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:${BuildDependenciesVersions.LIFECYCLE}"
+    const val CORE_KTX = "androidx.core:core-ktx:${BuildDependenciesVersions.CORE_KTX}"
+    const val FRAGMENT_KTX = "androidx.fragment:fragment-ktx:${BuildDependenciesVersions.FRAGMENT_KTX}"
+    const val CONSTRAIN_LAYOUT = "androidx.constraintlayout:constraintlayout:${BuildDependenciesVersions.CONSTRAIN_LAYOUT}"
+    const val DAGGER = "com.google.dagger:dagger:${BuildDependenciesVersions.DAGGER}"
+    const val TIMBER = "com.jakewharton.timber:timber:${BuildDependenciesVersions.TIMBER}"
+    const val LOGGING = "com.squareup.okhttp3:logging-interceptor:${BuildDependenciesVersions.LOGGING}"
+    const val MOSHI = "com.squareup.moshi:moshi:${BuildDependenciesVersions.MOSHI}"
+    const val MOSHI_KTX = "com.squareup.moshi:moshi-kotlin:${BuildDependenciesVersions.MOSHI}"
+    const val GLIDE = "com.github.bumptech.glide:glide:${BuildDependenciesVersions.GLIDE}"
+    const val CRASHLYTICS = "com.crashlytics.sdk.android:crashlytics:${BuildDependenciesVersions.CRASHLYTICS}"
+    const val PLAY_CORE = "com.google.android.play:core:${BuildDependenciesVersions.PLAY_CORE}"
+    const val FIREBASE = "com.google.firebase:firebase-analytics:${BuildDependenciesVersions.FIREBASE}"
+    const val GMS_MAPS = "com.google.android.gms:play-services-maps:${BuildDependenciesVersions.GMS_MAPS}"
+}
