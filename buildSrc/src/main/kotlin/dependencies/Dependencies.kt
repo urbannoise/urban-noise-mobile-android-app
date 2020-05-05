90d80881d9f1e8935528af2dbe8297251ebc61ba
Jose Paya <josepaya17@gmail.com>
2020-05-05 23:17:57 +0200

    Change Firebase Analytics dependency to KTX version

--- buildSrc/src/main/kotlin/dependencies/Dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies/Dependencies.kt
@@ -43,6 +43,6 @@ object Dependencies {
     const val MOSHI_KTX = "com.squareup.moshi:moshi-kotlin:${BuildDependenciesVersions.MOSHI}"
     const val GLIDE = "com.github.bumptech.glide:glide:${BuildDependenciesVersions.GLIDE}"
     const val PLAY_CORE = "com.google.android.play:core:${BuildDependenciesVersions.PLAY_CORE}"
-    const val FIREBASE_ANALYTICS = "com.google.firebase:firebase-analytics:${BuildDependenciesVersions.FIREBASE}"
+    const val FIREBASE_ANALYTICS = "com.google.firebase:firebase-analytics-ktx:${BuildDependenciesVersions.FIREBASE_ANALYTICS}"
     const val GMS_MAPS = "com.google.android.gms:play-services-maps:${BuildDependenciesVersions.GMS_MAPS}"
 }
