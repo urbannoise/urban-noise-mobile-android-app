5da703d981f0b716ad9463be69903b0c46dea630
Madalin Valceleanu <devmobile@vmadalin.com>
2020-05-19 19:35:57 +0100

    Start implement publisher tripet-t for google play release

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -47,6 +47,7 @@ object PluginsVersions {
     const val GOOGLE_SERVICES = "4.3.3"
     const val FIREBASE_CRASHLYTICS = "2.0.0"
     const val FIREBASE_PERFORMANCE = "1.3.1"
+    const val PLAY_PUBLISHER = "2.7.5"
 }
 
 dependencies {
@@ -64,4 +65,5 @@ dependencies {
     implementation("com.google.gms:google-services:${PluginsVersions.GOOGLE_SERVICES}")
     implementation("com.google.firebase:firebase-crashlytics-gradle:${PluginsVersions.FIREBASE_CRASHLYTICS}")
     implementation("com.google.firebase:perf-plugin:${PluginsVersions.FIREBASE_PERFORMANCE}")
+    implementation("com.github.triplet.gradle:play-publisher:${PluginsVersions.PLAY_PUBLISHER}")
 }
